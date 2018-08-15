ee558c2b9e1ae729fd222256952f248c8c900efa
soapia <sofia.ongele@ealas.org>
2018-08-14 21:22:50 -0400

    Initial Commit

---
+++ firebase test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  firebase test
+//
+//  Created by Sofia Ongele on 8/14/18.
+//  Copyright © 2018 Sofia Ongele. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
